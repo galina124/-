7605ce815a1ca59b6cccca020db008ad8025559f
galina124 <164925895+galina124@users.noreply.github.com>
2025-01-05 20:14:57 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+import mach_fake
+import mach_true
+
+from mach_fake import divide as fake_divide
+from mach_true import divide as true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
